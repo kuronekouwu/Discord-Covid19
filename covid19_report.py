234b353ec95c4a878b9d2d9981cf9dbe5ca30e3e
Mr. Wan <unturnedmc1@gmail.com>
2020-04-10 00:05:20 +0700

    Discord COVID-19 Command

---
+++ covid19_report.py
@@ -0,0 +1,44 @@
+import discord
+import aiohttp 
+import json
+from discord.ext import commands
+
+TOKEN = '' #เอา Token Bot มาใช้งานได้เล๊ยย!!
+PREFIX = '?>' #คำนำหน้า
+
+bot = commands.Bot(command_prefix=PREFIX)
+
+@bot.event 
+async def on_ready() :
+	print(f"Bot {bot.user.name} has started!")
+
+async def get_data_url(url) :
+	async with aiohttp.ClientSession() as session :
+		html = await fetch(session, url)
+
+		return html
+
+async def fetch(session, url) :
+	async with session.get(url) as respones :
+		return await respones.text()
+
+@bot.command()
+async def covid19(ctx) :
+	thai = await get_data_url('https://covid19.th-stat.com/api/open/timeline')
+	thai = json.loads(thai)
+
+	e = discord.Embed(
+		title="ข้อมูล COVID-19",
+		description=f"อัพเดตล่าลุดเมื่อ {thai['UpdateDate']}",
+		color=0xf2466c
+	)
+
+	e.add_field(name=':thermometer_face: ผู้ป่วยสะสม',value=f"{thai['Data'][-1]['Confirmed']} คน")
+	e.add_field(name=':mask: ผู้ป่วยรายใหม่',value=f"{thai['Data'][-1]['NewConfirmed']} คน")
+	e.add_field(name=':homes:  ผู้ป่วยรักษาหายแล้ว',value=f"{thai['Data'][-1]['Recovered']} คน")
+	e.add_field(name=':skull_crossbones: ผู้ป่วยเสียชีวิต',value=f"{thai['Data'][-1]['Deaths']} คน")
+	e.set_footer(text=f'''ข้อมูลจาก {thai["Source"]}''')
+
+	await ctx.send(embed=e)
+
+bot.run(TOKEN)
